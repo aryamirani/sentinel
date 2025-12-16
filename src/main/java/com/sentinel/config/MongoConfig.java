6b6f170f2c00dddea62755f8a9434aac5d8136c5
Arya Mirani <aryamirani06@gmail.com>
2025-12-16 08:24:13 +0530

    working on mongoconfig

---
+++ src/main/java/com/sentinel/config/MongoConfig.java
@@ -0,0 +1,36 @@
+package com.sentinel.config;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@Configuration
+@EnableReactiveMongoRepositories(basePackages = "com.sentinel.repository")
+public class MongoConfig extends AbstractReactiveMongoConfiguration {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
+    @Override
+    public MongoClient reactiveMongoClient() {
+        return MongoClients.create(mongoUri);
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return databaseName;
+    }
+
+    @Bean
+    public ReactiveMongoTemplate reactiveMongoTemplate() {
+        return new ReactiveMongoTemplate(reactiveMongoClient(), getDatabaseName());
+    }
+}
